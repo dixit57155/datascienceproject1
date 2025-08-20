49cef526e7447b4fe941c1b0a42733a8ff05be32
dixit <dixit57155@example.com>
2025-08-20 12:56:48 +0530

    project structure ready

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+project_name="datascienceproject1"
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"tests/{project_name}/utils/__init__.py",
+    f"tests/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yml",
+    "params.yml",
+    "schema.yml",
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "research/research.ipynb",
+    "templates/index.html"
+
+
+]
+
+for filename in list_of_files:
+    filepath=Path(filename)
+    filedir,filename = os.path.split(filepath)
+    
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file : {filename}")
+
+
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath) == 0)):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Created file: {filename} at {filedir}")
+
+    else:
+        logging.info(f"File already exists: {filename}.")
