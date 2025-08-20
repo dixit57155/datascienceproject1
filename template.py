03fab5e90753261d6adcf5ac0fa4b24b8cd18c40
dixit <dixit57155@example.com>
2025-08-20 13:26:39 +0530

    set template folder structure (util folder)

--- template.py
+++ template.py
@@ -10,8 +10,8 @@ list_of_files=[
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"tests/{project_name}/utils/__init__.py",
-    f"tests/{project_name}/utils/common.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
@@ -46,3 +46,6 @@ for filename in list_of_files:
 
     else:
         logging.info(f"File already exists: {filename}.")
+
+
+
